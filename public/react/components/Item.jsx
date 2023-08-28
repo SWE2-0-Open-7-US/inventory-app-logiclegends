fd3398faf4e8d5246f93c8136e2ada79bb7bbe25
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-28 12:35:20 -0400

    Single item and item list updated styling with react bootstrap

--- public/react/components/Item.jsx
+++ public/react/components/Item.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import apiURL from '../api';
 import { EditItem } from './EditItem';
+import { itemPriceFormatter } from '../utils/utils';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import Collapse from 'react-bootstrap/Collapse';
+
 
 
 export const Item = () => {
@@ -35,21 +44,43 @@ export const Item = () => {
     }
 
     return (
-        <div>
+        <Container className="py-4">
             {item && (
-                <>
-                    <h2>{item.name}</h2>
-                    <img src={item.image} alt={`${item.name}`} />
-                    <p>{item.description}</p>
-                    <p>Price: ${item.price}</p>
-                    <p>Category: {item.category}</p>
-                    <button onClick={() => setIsEdit(!isEdit)} >Edit Item</button>
-                    <button onClick={() => deleteItem()} >Delete Item</button>
-                    {isEdit && (
-                        <EditItem setIsEdit={setIsEdit} item={item} id={id} />
-                    )}
-                </>
+                <div>
+                    <Card>
+                        <Card.Header><h2>{item.name}</h2></Card.Header>
+                        <Card.Body>
+                            <Row className='align-items-center'>
+                                <Col md={6} xs={12} className='border-right-responsive border-bottom-responsive'>
+                                    <Image src={item.image} alt={`${item.name}`} fluid />
+                                </Col>
+                                <Col md={6} xs={12}>
+                                    <Card.Text>{item.description}</Card.Text>
+                                    <Card.Text>Price: {itemPriceFormatter.format(item.price)}</Card.Text>
+                                    <Card.Text>Category: {item.category}</Card.Text>
+                                    <Button
+                                        variant="primary"
+                                        onClick={() => setIsEdit(!isEdit)}
+                                        aria-controls="edit-item-collapse"
+                                        aria-expanded={isEdit}
+                                    >
+                                        {isEdit ? "Cancel Edit" : "Edit Item"}
+                                    </Button>
+                                    <Button variant="danger" onClick={() => deleteItem()} className="ml-2">Delete Item</Button>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                        <Collapse in={isEdit}>
+                            <div id="edit-item-collapse">
+                                <Card.Footer>
+
+                                    <EditItem setIsEdit={setIsEdit} item={item} id={id} />
+                                </Card.Footer>
+                            </div>
+                        </Collapse>
+                    </Card>
+                </div>
             )}
-        </div>
+        </Container>
     )
 }
