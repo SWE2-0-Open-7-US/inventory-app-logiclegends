c226e99642e58dcbca2fd4f4ada7940aa5c897fd
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-29 00:51:32 -0400

    Filter/search back and front-end connected, styled with react-bootstrap

--- public/react/components/Filter.jsx
+++ public/react/components/Filter.jsx
@@ -1,32 +1,63 @@
 import React, { useState } from 'react';
+import { Button, Form, Stack, Row, Col } from 'react-bootstrap';
 
 function Filter({ filterItems }) {
-    const [criteria, setCriteria] = useState('name'); 
+    const [criteria, setCriteria] = useState('name');
     const [query, setQuery] = useState('');
+    const [timeoutId, setTimeoutId] = useState(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        filterItems(criteria, query);
+    const handleFilterChange = (newQuery) => {
+        setQuery(newQuery);
+
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        const newTimeoutId = setTimeout(() => {
+            filterItems(criteria, newQuery);
+        }, 200);
+
+        setTimeoutId(newTimeoutId);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <select 
-                value={criteria} 
-                onChange={e => setCriteria(e.target.value)}
-            >
-                <option value="name">Name</option>
-                <option value="category">Category</option>
-                <option value="description">Description</option>
-            </select>
-            <input 
-                type="text" 
-                placeholder="Filter items..."
-                value={query}
-                onChange={e => setQuery(e.target.value)}
-            />
-            <button type="submit">Filter</button>
-        </form>
+        <Row>
+            <Col lg={{ span: 5, offset: 7 }} md={12}>
+                <Stack direction="horizontal" className="w-100 mt-4">
+                    <Form.Select
+                        value={criteria}
+                        onChange={e => {
+                            setCriteria(e.target.value)
+                            if (query) {
+                                filterItems(e.target.value, query)
+                            }
+                        }}
+                        className="flex-grow-1"
+                    >
+                        <option value="name">Name</option>
+                        <option value="category">Category</option>
+                        <option value="description">Description</option>
+                    </Form.Select>
+
+                    <Form.Control
+                        type="text"
+                        placeholder="Search items..."
+                        value={query}
+                        onChange={(e) => handleFilterChange(e.target.value)}
+                        className="flex-grow-1 mx-2"
+                    />
+
+                    <Button variant="secondary" onClick={() => {
+                        setQuery('');
+                        if (timeoutId) {
+                            clearTimeout(timeoutId);
+                        }
+                        filterItems(criteria, '');
+                    }}>Reset</Button>
+                </Stack>
+            </Col>
+        </Row>
+
     );
 }
 
--- public/react/components/ItemList.jsx
+++ public/react/components/ItemList.jsx
@@ -13,48 +13,51 @@ import Filter from './Filter';
 
 function ItemList() {
   const navigate = useNavigate()
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([{name: '', description: '', id: 0, price: 0, image: ''}]);
 
   useEffect(() => {
     // Fetch items from API
+    fetchItems()
+  }, []);
+
+  const fetchItems = async () => {
     fetch(`${apiURL}/items`)
       .then(response => response.json())
       .then(data => setItems(data))
       .catch(error => console.error('Error fetching items:', error));
-  }, []);
+  };
 
   const filterItems = async (criteria, query) => {
-    const response = await fetch(`${apiURL}/items/filter?criteria=${criteria}&query=${query}`);
-    const data = await response.json();
-    setItems(data);
+    try {
+      const response = await fetch(`${apiURL}/items/filter?criteria=${criteria}&query=${query}`);
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.log("Error filtering", error);
+    }
   };
 
   return (
     <>
-      {
-        items && (
-          <Container>
-            <Row>
-              <Col>
-            <Filter filterItems={filterItems} />
-              </Col>
-            </Row>
-            <Row>
-              {items.map(item => (
-                <Col md={4} sm={6} xs={12} key={item.id} className="my-4">
-                  <Card style={{ cursor: 'pointer', minHeight: '100%' }} onClick={() => navigate(`/items/${item.id}`)}>
-                    <Card.Img variant="top" src={item.image} alt={`${item.name}`} style={{ height: '200px', padding: '5%', objectFit: 'contain' }} />
-                    <Card.Body >
-                      <Card.Title style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{item.name}</Card.Title>
-                      <Card.Text>{itemPriceFormatter.format(item.price)}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </Container>
-        )
-      }
+      <Container>
+
+        <Filter filterItems={filterItems} resetItems={fetchItems} />
+
+        <Row>
+          {items.map(item => (
+            <Col md={4} sm={6} xs={12} key={item.id} className="mt-4">
+              <Card style={{ cursor: 'pointer', minHeight: '100%' }} onClick={() => navigate(`/items/${item.id}`)}>
+                <Card.Img variant="top" src={item.image} alt={`${item.name}`} style={{ height: '200px', padding: '5%', objectFit: 'contain' }} />
+                <Card.Body >
+                  <Card.Title style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{item.name}</Card.Title>
+                  <Card.Text>{itemPriceFormatter.format(item.price)}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+          { !items.length && <h3 className="my-4">No items to show</h3>}
+        </Row>
+      </Container>
     </>
   );
 }
